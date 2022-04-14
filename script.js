6612aace38c61713025d7cf1d1f1b3e988ce4e08
LLla6aJl <salimovvvv@mail.ru>
2022-04-14 14:54:41 +0300

    added alert

--- script.js
+++ script.js
@@ -13,23 +13,30 @@ function getRepo() {
     .then((response) => {
       return response.json();
     })
-    .then((response) => templateItems(response.items));
+    .then((response) => templateItems(response.items))
+    .catch((error) => {
+      alert("не получены данные от сервера или превышен лимит запросов");
+    });
 }
 
 function templateItems(items) {
   searchWrapper.innerHTML = "";
   const fragment = document.createDocumentFragment();
-  items.forEach((element) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    const article = document.createElement("a");
-    article.classList.add("card-text");
-    article.textContent = element.name;
-    card.appendChild(article);
-    card.addEventListener("click", () => createPost(element), { once: true });
-    fragment.appendChild(card);
-  });
-  searchWrapper.appendChild(fragment);
+  if (Array.isArray(items)) {
+    items.forEach((element) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const article = document.createElement("a");
+      article.classList.add("card-text");
+      article.textContent = element.name;
+      card.appendChild(article);
+      card.addEventListener("click", () => createPost(element), { once: true });
+      fragment.appendChild(card);
+    });
+    searchWrapper.appendChild(fragment);
+  } else {
+    alert("не получены данные от сервера или превышен лимит запросов");
+  }
 }
 
 function createPost(element) {
@@ -73,4 +80,4 @@ const debounce = (fn, debounceTime) => {
   };
 };
 
-inputBox.addEventListener("keyup", debounce(getRepo, 400));
+inputBox.addEventListener("keyup", debounce(getRepo, 600));
